210f683b80f5d200e08d4d966f0adbfa0369791e
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-09-19 08:00:00 +0200

    clean up

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
     hydra.url = "github:nixos/hydra";
-    #"github:nixos/hydra?rev=222a8047e4d29329d568207469fd8b30581843c0";
   };
 
   outputs = inputs@{ self, nixpkgs, agenix, hydra, }:
