12872c92e30794dc9a52a69046711255444c1a4f
Nadrieril <nadrieril+git@gmail.com>
2025-07-17 11:40:23 +0200

    Upgrade

--- switch.sh
+++ switch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nixos-rebuild switch -v --flake .#hacl-ci --build-host hacl@128.93.101.164 --target-host hacl@128.93.101.164 --use-remote-sudo
+nixos-rebuild switch -v --flake .#hacl-ci --build-host hacl@128.93.101.164 --target-host hacl@128.93.101.164 --sudo
